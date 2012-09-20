cadc85565481019cf2a85e387e08583c3f92fd7d
Adam Hammouda <adamhammouda3@gmail.com>
2012-09-20 18:32:31 -0500

    Removed logging temporarily

--- djpclient_example/settings.py
+++ djpclient_example/settings.py
@@ -220,6 +220,7 @@ BROKER_PASSWORD = DATABASES['default']['PASSWORD']
 # the site admins on every HTTP 500 error when DEBUG=False.
 # See http://docs.djangoproject.com/en/dev/topics/logging for
 # more details on how to customize your logging configuration.
+'''
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
@@ -252,3 +253,4 @@ LOGGING = {
         },
     }
 }
+'''
